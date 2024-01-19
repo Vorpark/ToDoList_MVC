8605efbe29d2540fbb6091907ef99f2ce6f48fc9
Vorpark <nik.vorpark@mail.ru>
2024-01-19 16:11:47 +0300

    Первоначальная настройка Task/Index

--- ToDoList.Domain/Enum/Priority.cs
+++ ToDoList.Domain/Enum/Priority.cs
@@ -1,9 +1,14 @@
-﻿namespace ToDoList.Domain.Enum
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Domain.Enum
 {
     public enum Priority
     {
-        Easy = 1, 
+        [Display(Name = "Простая")]
+        Easy = 1,
+        [Display(Name = "Важная")]
         Medium = 2,
+        [Display(Name = "Критичная")]
         Hard = 3
     }
 }
