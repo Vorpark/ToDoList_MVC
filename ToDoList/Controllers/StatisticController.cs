00c2ba7f219dc22599503ac8c497acd26f784d94
Vorpark <nik.vorpark@mail.ru>
2024-01-23 19:42:05 +0300

    Инициализация нового контроллера Statistic

---
+++ ToDoList/Controllers/StatisticController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoList.DAL.Repository.IRepository;
+
+namespace ToDoList.Controllers
+{
+    public class StatisticController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public StatisticController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
